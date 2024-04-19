dbe0a026e7cf65e903dc208a88cb4e90f68e3421
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2024-04-19 19:01:09 +0530

    added flask support

--- task1/weight_dl.py
+++ task1/weight_dl.py
@@ -2,7 +2,7 @@ import gdown
 
 weight_gdrive = "https://drive.google.com/uc?id=1fj1wAI1Xj7ykWtayKAIXZSZ4FkHoqGqU"
 total_weight = "https://drive.google.com/uc?id=17rfov8knJC6TyXiBHI4HR4ks49YI7ew6"
-dst_unzip = "/opt/app/"
-dst_unzip_totalseg = "/opt/app/"
+dst_unzip = "/home/app/"
+dst_unzip_totalseg = "/home/app/"
 gdown.download(weight_gdrive, dst_unzip)
 gdown.download(total_weight, dst_unzip_totalseg)
