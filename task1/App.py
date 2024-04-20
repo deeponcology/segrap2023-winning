e609de4edee2afd54eeda68ff96faadc773dfe47
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2024-04-20 12:57:49 +0530

    added flask support

--- task1/App.py
+++ task1/App.py
@@ -96,4 +96,4 @@ def process_data():
     return segmented_file
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000,debug=False,threaded=True)
+    app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
